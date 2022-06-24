f95ae88de6e8412560169aaef282377aae2a9730
GiselyKC <gikavalli@gmail.com>
2022-06-24 18:14:59 -0300

    feat: adicionado express-async-errors no arquivo api.js

--- src/api.js
+++ src/api.js
@@ -1,10 +1,15 @@
 const express = require('express');
+require('express-async-errors');
+const middlewareErrors = require('./middlewares/error');
 
 // ...
 
 const app = express();
+const router = require('./router');
 
 app.use(express.json());
+app.use('/', router);
+app.use(middlewareErrors);
 
 // ...
 
