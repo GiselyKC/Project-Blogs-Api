480df37a5257c0046dd4ce7d3e7d726639ca4b44
GiselyKC <gikavalli@gmail.com>
2022-06-24 18:16:52 -0300

    feat: criado o arquivo generateToken.js

---
+++ src/helpers/generateToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+const config = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
+
+const generateJWT = (email) => {
+  const payload = { email };
+
+  const token = jwt.sign(payload, JWT_SECRET, config);
+
+  return token;
+};
+
+module.exports = generateJWT;
