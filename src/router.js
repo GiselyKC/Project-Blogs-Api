4a95e75eb26ad25d5cf369edc081e95d3fbd2fb7
GiselyKC <gikavalli@gmail.com>
2022-06-25 13:49:02 -0300

    feat: adicionado middlaware do endpoint get da rota /user/:id

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const loginController = require('./controllers/loginController');
-const { userPost, userGet } = require('./controllers/userController');
+const { userPost, userGet, userGetById } = require('./controllers/userController');
 const validateLogin = require('./middlewares/loginMiddleware');
 const {
   validateDisplayName,
@@ -14,5 +14,6 @@ const routers = express.Router();
 routers.post('/login', validateLogin, loginController);
 routers.post('/user', validateDisplayName, validateEmail, validatePassword, userPost);
 routers.get('/user', authMiddleware, userGet);
+routers.get('/user/:id', authMiddleware, userGetById);
 
 module.exports = routers;
