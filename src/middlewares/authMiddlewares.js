9154a1eca2675acd3f58a52d6a823437c929a1e8
GiselyKC <gikavalli@gmail.com>
2022-06-25 00:12:07 -0300

    feat: criado middlewares de validação de token

---
+++ src/middlewares/authMiddlewares.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+const authMiddleware = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+    
+  try {
+    jwt.verify(authorization, JWT_SECRET);
+    return next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
+
+module.exports = authMiddleware;
