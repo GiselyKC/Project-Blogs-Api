b82910d8ddd8fb929a0aa3bbdf92bb0f9bce7426
GiselyKC <gikavalli@gmail.com>
2022-06-24 23:43:17 -0300

    feat: adicionado middlaware do endpoint post da rota user

--- src/router.js
+++ src/router.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const loginController = require('./controllers/loginController');
-// const userController = require('./controllers/userController');
+const { userController } = require('./controllers/userController');
 const validateLogin = require('./middlewares/loginMiddleware');
+const {
+  validateDisplayName,
+  validateEmail,
+  validatePassword,
+} = require('./middlewares/userMiddleware');
 
 const routers = express.Router();
 
 routers.post('/login', validateLogin, loginController);
+routers.post('/user', validateDisplayName, validateEmail, validatePassword, userController);
 
 module.exports = routers;
